329ae0d73bf8ac6538cea8acaf2b40558a9dbc41
denis <denismarchena9@gmail.com>
2025-07-30 20:34:32 -0600

    Se creo la clase Transmisiones y enums VelcidadesMaximas, EsatdosMarcha
    Se ceo la clase transmiciones para controlar las marchas de vehiculo la clase transmisiones tiene 3 atributos, el embrague, marcha y la velocidad maxima que alcanza esa marcha especifica, siendo estos ultimos 2 atributos representados por los anums, los anum representa las marchas del 1 al 5, el estado de neutro y reversa del vehiculo, el enum limita segun cada estado de la marcha las velocidades maximas que puede alcanzar el kilometraje del vehiculo, la clase transmisiones tiene metodos para cambiar a cada marcha especifica y para presionar o soltar el embrague

---
+++ src/Vehiculo/Transmisiones/Transmision.java
@@ -0,0 +1,111 @@
+package Vehiculos.Transmisiones;
+
+/**
+ *
+ * @author denis
+ */
+public class Transmision {
+//INICIALIZAR
+
+//ATRIBUTOS    
+    private boolean Embrague;
+    private EstadosMarcha Marcha;
+    private VelocidadesMaximas VelocidadMaxMarcha;
+
+//METODOS GET
+    public boolean isEmbrague() {
+        return Embrague;
+    }
+
+    public EstadosMarcha getMarcha() {
+        return Marcha;
+    }
+
+    public VelocidadesMaximas getVelocidadMaxMarcha() {
+        return VelocidadMaxMarcha;
+    }
+    
+//METODOS SET
+    public void setEmbrague(boolean Embrague) {
+        this.Embrague = Embrague;
+    }
+
+    public void setMarcha(EstadosMarcha Marcha) {
+        this.Marcha = Marcha;
+    }
+
+    public void setVelocidadMaxMarcha(VelocidadesMaximas VelocidadMaxMarcha) {
+        this.VelocidadMaxMarcha = VelocidadMaxMarcha;
+    }
+    
+//CONSTRUCTOR
+
+    public Transmision() {
+        this.Embrague = false;
+        this.Marcha = Marcha.NEUTRO;
+        this.VelocidadMaxMarcha = VelocidadMaxMarcha.NEUTRO;
+    }
+
+    
+//OTROS
+    //Timmers
+    
+    //Metodos
+    public void SoltarEmbrague() {
+        Embrague = false;
+    }
+    
+    public void PresionarEmbrague(){
+        Embrague = true;
+    }
+    
+    public void setNeutro (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.NEUTRO;
+        VelocidadMaxMarcha = VelocidadesMaximas.NEUTRO;
+        }
+    }
+    
+    public void setMarcha1 (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.MARCHA1;
+        VelocidadMaxMarcha = VelocidadesMaximas.KmH20;
+        }
+    }
+    
+    public void setMarcha2 (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.MARCHA2;
+        VelocidadMaxMarcha = VelocidadesMaximas.KmH40;
+        }
+    }
+    
+    public void setrMarcha3 (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.MARCHA3;
+        VelocidadMaxMarcha = VelocidadesMaximas.KmH60;
+        }       
+    }
+    
+    public void setMarcha4 (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.MARCHA4;
+        VelocidadMaxMarcha = VelocidadesMaximas.KmH80;
+        }
+    }
+    
+    public void setMarcha5 (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.MARCHA5;
+        VelocidadMaxMarcha = VelocidadesMaximas.KmH300;
+        }
+    }
+    
+    public void setReversa (){
+        if (Embrague) {
+        Marcha = EstadosMarcha.REVERSA;
+        VelocidadMaxMarcha = VelocidadesMaximas.NEUTRO;
+        }
+    }
+    
+}
